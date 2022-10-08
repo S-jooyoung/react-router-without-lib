dad6d7b27dde64a7856f8858f39a8da1c0092451
S-jooyoung <jooyoung.dev@gmail.com>
2022-10-08 00:30:00 +0000

    Fix: change setting && main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// custom-components
+import Router from "./components/router";
+import Route from "./components/route";
+
+// pages
+import About from "./pages/about";
+import Root from "./pages/root";
+
+const container = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/" component={<Root />} />
+      <Route path="/about" component={<About />} />
+    </Router>
   </React.StrictMode>
-)
+);
